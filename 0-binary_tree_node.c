035b4227054b91d62842eeb65d68d4d22b58c5a1
arsicbt <chobertarsinoe@gmail.com>
2025-08-04 12:00:08 +0000

    fix: fix the conflit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,17 +3,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    parent->left = new_node;     
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    return (new_node);
+	new_node->n = value;
+	new_node->parent = parent;
+	parent->left = new_node;
+
+	return (new_node);
 }
