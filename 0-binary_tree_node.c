4768d8950d87ecbf2d21932288c2b5a526a0eaa3
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 <11534@holbertonstudents.com>
2025-08-04 11:48:58 +0200

    fix node left and right to NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     
     new_node->n = value;
     new_node->parent = parent;
-    parent->left = new_node;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    parent->left = new_node;     
 
     return (new_node);
 }
