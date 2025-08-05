d5da00e9ba223b6f97ab9f17b96d91e64b691ec3
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 <11534@holbertonstudents.com>
2025-08-05 10:50:22 +0200

    fix betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_h, right_h;
+	size_t left_h, right_h;
 
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-        return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    left_h = 1 + binary_tree_height(tree->left);
-    right_h = 1 + binary_tree_height(tree->right);
+	left_h = 1 + binary_tree_height(tree->left);
+	right_h = 1 + binary_tree_height(tree->right);
 
-    if (left_h > right_h)
-        return (left_h);
-    else
-        return (right_h);
+	if (left_h > right_h)
+		return (left_h);
+	else
+		return (right_h);
 }
